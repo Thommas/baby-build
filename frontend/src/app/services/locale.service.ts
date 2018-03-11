c73a7a4dd0044d3716a5fa16c2d66048ae25f8af
Thomas Bullier <thomasbullier@gmail.com>
2018-03-11 14:31:27 +0000

    Fixed locale redirect Cannot use localStorage cross domain

--- frontend/src/app/services/locale.service.ts
+++ frontend/src/app/services/locale.service.ts
@@ -22,32 +22,28 @@ export class LocaleService {
   public constructor(private browserService: BrowserService) {}
 
   public detectLocale() {
-    let locale = 'en';
-
-    const localStorage = this.browserService.localStorage;
-    if (localStorage && this.browserService.localStorage.getItem(this.LOCALE_KEY)) {
-      locale = this.browserService.localStorage.getItem(this.LOCALE_KEY);
-    } else if (this.browserService.language) {
-      locale = this.browserService.language;
-    }
+    let locale = this.browserService.language;
     if (this.LOCALES.map(locale => locale.value).indexOf(locale) === -1) {
       locale = 'en';
     }
-
-    this.setLocale(locale);
+    if (locale !== 'en') {
+      this.setLocale(locale);
+    }
   }
 
   public setLocale(newLocale) {
-    const localStorage = this.browserService.localStorage;
-    if (localStorage) {
-      this.browserService.localStorage.setItem(this.LOCALE_KEY, newLocale);
-    }
-
     let subdomain = newLocale;
     if (subdomain === 'en') {
-      subdomain = 'www';
+      subdomain = '';
+    } else {
+      subdomain += '.';
     }
-    this.browserService.window.location.href = subdomain + '.' + environment.baseDomainUrl;
+    const host = this.browserService.window.location.host;
+    const localeHost = subdomain + environment.baseDomainUrl;
+    if (host !== localeHost) {
+      const protocol = this.browserService.window.location.protocol;
+      this.browserService.window.location.href = protocol + '//' + localeHost;
+    }
   }
 
   public get locales() {
